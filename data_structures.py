69b57fc511b9b1669ac9f06923eed34ee49a478c
Mack Damavandi <mack.damavandi@gmail.com>
2019-10-16 19:44:52 -0600

    update: added remove method to the class

--- data_structures.py
+++ data_structures.py
@@ -48,6 +48,20 @@ class ChainingHashTable:
             # the key wasn't found
             return None
 
+    def remove(self, key):
+        """
+        removes an item with matching key from the has table
+        :param key: item to be removed
+        :return: None
+        """
+        # get the bucket list this item will be removed from
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
+
+        # remove the item from the bucket list if it is present
+        if key in bucket_list:
+            bucket_list.remove(key)
+
 if __name__ == '__main__':
     # Main program to create a couple of HashTable objects, showing how
     # the 'capacity' constructor argument works.
@@ -57,6 +71,7 @@ if __name__ == '__main__':
     ht1.insert(10)
     ht1.insert('car')
     ht1.insert(5.2)
+    ht1.remove(5.2)
 
     print('ht1:', ht1.table)
 
