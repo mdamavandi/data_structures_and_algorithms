330625bd97cef266aa0bbd1df5b53e1a55706ebf
Mack Damavandi <mack.damavandi@gmail.com>
2019-10-16 20:25:02 -0600

    update: added search and remove methods to LinearProbingHashTable

--- data_structures.py
+++ data_structures.py
@@ -110,29 +110,78 @@ class LinearProbingHashTable:
 
         # the entire table was full and the key could not be inserted
         return False
+
+    def remove(self, key):
+        """
+        removes an item with a matching key from the hash table
+        :param key: item to be removed
+        :return: None
+        """
+
+        # find initial bucket to be looked in
+        bucket = hash(key) % len(self.table)
+        buckets_probed = 0
+
+        # start linearly probing the buckets
+        while self.table[bucket] is not self.EMPTY_SINCE_START and buckets_probed < len(self.table):
+            # if we find the item, set that bucket to EMPTY_AFTER_REMOVAL
+            if self.table[bucket] == key:
+                self.table[bucket] = self.EMPTY_AFTER_REMOVAL
+
+            # the bucket was occupied (now or previously), so continue probing
+            bucket = (bucket + 1) % len(self.table)
+            buckets_probed += 1
+
+    def search(self, key):
+        """
+        searches for an item with a matching key in the hash table
+        returns the item if found or None if not found
+        :param key: item to be searched for
+        :return: the found item or None if not found
+        """
+
+        # find initial bucket to be looked in
+        bucket = hash(key) % len(self.table)
+        buckets_probed = 0
+
+        # start linearly probing the buckets
+        while self.table[bucket] is not self.EMPTY_SINCE_START and buckets_probed < len(self.table):
+            # if we find the item, return it
+            if self.table[bucket] == key:
+                return self.table[bucket]
+
+            # the bucket was occupied (now or previously), so continue probing
+            bucket = (bucket + 1) % len(self.table)
+            buckets_probed += 1
+
+        # the entire table was probed or an EMPTY_SINCE_START bucket was found
+        return None
+
 if __name__ == '__main__':
     # Main program to create a couple of HashTable objects
     ht1 = ChainingHashTable()
-    print('ht1:', ht1.table)
+    print(f'ht1: {ht1.table}')
 
     ht1.insert(10)
     ht1.insert('car')
     ht1.insert(5.2)
     ht1.remove(5.2)
 
-    print('ht1:', ht1.table)
+    print(f'ht1: {ht1.table}')
 
     ht2 = ChainingHashTable(5)
-    print('ht2:', ht2.table)
+    print(f'ht2: {ht2.table}')
 
     ht2.insert(10)
     ht2.insert('car')
     ht2.insert(5.2)
 
-    print('ht2:', ht2.table)
+    print(f'ht2: {ht2.table}')
 
     ht3 = LinearProbingHashTable()
     ht3.insert('foo')
     ht3.insert('bar')
+    print(ht3.search('bar'))
+    ht3.remove('bar')
 
-    print('ht3:', ht3.table)
+    print(f'ht3: {ht3.table}')
