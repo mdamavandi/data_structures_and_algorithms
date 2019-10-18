47154a7269ca73d2a2725c918d24942bdf92e8fc
Mack Damavandi <mack.damavandi@gmail.com>
2019-10-17 20:46:44 -0600

    update: added insert method to BinarySearchTree and main namespace to test BinarySearchTree

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -14,8 +14,8 @@ class Node:
         self.right = None
 
 class BinarySearchTree:
-    def __init__(self):
-        self.root = None
+    def __init__(self, root=None):
+        self.root = root
 
     def search(self, desired_key):
         """
@@ -43,4 +43,59 @@ class BinarySearchTree:
         # the key was not found in the tree
         return None
 
+    def insert(self, node):
+        """
+        inserts a new node into the BST
+        :param node: node to be inserted
+        :return: None
+        """
+        # check if the tree is empty
+        if self.root is None:
+            self.root = node
+        else:
+            current_node = self.root
+            while current_node is not None:
+                if node.key < current_node.key:
+                    # if there is no left child, add the new node here;
+                    # otherwise, repeat from the left child
+                    if current_node.left is None:
+                        current_node.left = node
+                        current_node = None
+                    else:
+                        current_node = current_node.left
+                else:
+                    # if there is no right child, add the new node here;
+                    # otherwise repeat from the right child
+                    if current_node.right is None:
+                        current_node.right = node
+                        current_node = None
+                    else:
+                        current_node = current_node.right
+
 if __name__ == '__main__':
+    tree = BinarySearchTree()
+    node_a = Node(17)
+    node_b = Node(32)
+    node_c = Node(10)
+    node_d = Node(3)
+    node_e = Node(21)
+
+    tree.insert(node_a)
+    tree.insert(node_b)
+    tree.insert(node_c)
+    tree.insert(node_d)
+    tree.insert(node_e)
+
+    # search for key 3
+    found_node = tree.search(3)
+    if found_node is not None:
+        print(f'Found node with key = {found_node.key}.')
+    else:
+        print('Key 3 not found.')
+
+    # search for key 99
+    found_node = tree.search(99)
+    if found_node is not None:
+        print(f'Found node with key = {found_node.key}.')
+    else:
+        print('Key 99 not found.')
