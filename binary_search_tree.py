3d907b4dcf3bdb54cdb51655eb261e5ad45fde30
Mack Damavandi <mack.damavandi@gmail.com>
2019-10-17 20:34:06 -0600

    update: added search method to BinarySearchTree

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -16,3 +16,31 @@ class Node:
 class BinarySearchTree:
     def __init__(self):
         self.root = None
+
+    def search(self, desired_key):
+        """
+        searches for the desired_key in the BST
+        :param desired_key: item to be found
+        :return: the item if found, else None
+        """
+        current_node = self.root
+
+        while current_node is not None:
+            # return the node if the key matches
+            if current_node.key == desired_key:
+                return current_node
+
+            # navigate to the left if the search key is
+            # less than the node's key
+            elif desired_key < current_node.key:
+                current_node = current_node.left
+
+            # navigate to the right if the search key is
+            # greater than the node's key
+            else:
+                current_node = current_node.right
+
+        # the key was not found in the tree
+        return None
+
+if __name__ == '__main__':
