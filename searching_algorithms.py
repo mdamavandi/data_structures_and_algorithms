46e5a144817ddf6735efc38c0735ebef518b2fea
Mack Damavandi <mack.damavandi@gmail.com>
2019-10-22 20:10:47 -0600

    update: added binary_search()

--- searching_algorithms.py
+++ searching_algorithms.py
@@ -14,6 +14,37 @@ def linear_search(numbers, key):
     # key was not found in numbers
     return -1
 
+def binary_search(numbers, key):
+    """
+    find the location of some key in a list or indicate the key is not in the list
+    :param numbers: list to be searched
+    :param key: item to be found
+    :return: index of key in numbers or -1 if not found
+    """
+    # variables to hold the low, middle, and high indices of the areas being searched
+    # starts with entire range
+    low = 0
+    mid = len(numbers) // 2
+    high = len(numbers) - 1
+
+    # loop until 'low' passes 'high'
+    while (high >= low):
+        # calculate the middle index
+        mid = (high + low) // 2
+
+        # cut the range to either the left or right half
+        # unless numbers[mid] is the key
+        if (numbers[mid] < key):
+            low = mid + 1
+
+        elif (numbers[mid] > key):
+            high = mid - 1
+
+        else:
+            return mid
+    # not found
+    return -1
+
 if __name__ == '__main__':
     numbers = [
         2,
@@ -27,8 +58,16 @@ if __name__ == '__main__':
     ]
     print(f'NUMBERS: {numbers}')
 
-    key = int(input('Enter an integer value: '))
+    key = int(input('Linear search; enter an integer value: '))
     key_index = linear_search(numbers, key)
+
+    if (key_index == -1):
+        print(f'{key} was not found.')
+    else:
+        print(f'Found {key} at index {key_index}.')
+
+    key = int(input('Binary search; enter an integer value: '))
+    key_index = binary_search(numbers, key)
 
     if (key_index == -1):
         print(f'{key} was not found.')
