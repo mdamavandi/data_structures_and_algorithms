7d25e56770001ab32a51a35f1ed6d3fa0a1f1193
Mack Damavandi <mack.damavandi@gmail.com>
2019-10-22 20:55:39 -0600

    update: added partition() and quicksort()

--- sorthing_algorithms.py
+++ sorthing_algorithms.py
@@ -61,7 +61,66 @@ def shell_sort(numbers, gap_values):
         for i in range(gap_value):
             insertion_sort_interleaved(numbers, i, gap_value)
 
+def partition(numbers, start_index, end_index):
+    """
+    part of quicksort algorithm, works by partitioning section of unsorted list
+    into left part and right part based on a pivot
+    :param numbers: list to be sorted
+    :param start_index:
+    :param end_index:
+    :return: last item in left part's index
+    """
+    # select the middle value as the pivot
+    midpoint = start_index + (end_index - start_index) // 2
+    pivot = numbers[midpoint]
 
+    # 'low' and 'high' start at the ends of the list segment
+    # and move towards each other
+    low = start_index
+    high = end_index
+
+    done = False
+    while not done:
+        # increment low while numbers[low] < pivot
+        while numbers[low] < pivot:
+            low += 1
+
+        # decrement high while pivot < numbers[high]
+        while pivot < numbers[high]:
+            high -= 1
+
+        # if low and high have crossed each other, the loop is done
+        # if not, the elements are swapped, low is incremented, and high is decremented
+        if low >= high:
+            done = True
+        else:
+            numbers[low], numbers[high] = numbers[high], numbers[low]
+            low += 1
+            high -= 1
+
+    # 'high' is the last index in the left segment
+    return high
+
+def quicksort(numbers, start_index, end_index):
+    """
+    uses recursion to sort the two parts of the list
+    :param numbers: list to be sorted
+    :param start_index:
+    :param end_index:
+    :return: None
+    """
+    # only attempt to sort the list segment if there are at least two elements
+    if end_index <= start_index:
+        return
+
+    # partition the list segment
+    high = partition(numbers, start_index, end_index)
+
+    # recursively sort the left segment
+    quicksort(numbers, start_index, high)
+
+    # recursively sort the right segment
+    quicksort(numbers, high+1, end_index)
 
 if __name__ == '__main__':
     numbers = [
@@ -110,3 +169,21 @@ if __name__ == '__main__':
     shell_sort(numbers, [4, 2, 1])
 
     print(f'SHELL SORTED: {numbers}')
+
+    numbers = [
+        12,
+        18,
+        3,
+        7,
+        32,
+        14,
+        91,
+        16,
+        8,
+        57,
+    ]
+
+    print(f'UNSORTED: {numbers}')
+
+    quicksort(numbers, 0, len(numbers)-1)
+    print(f'QUICKSORTED: {numbers}')
